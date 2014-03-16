c11ffbcbc4c948b2b7f253ef28e31fe4b9a9723b
Rob Weir <rweir@ertius.org>
2014-03-16 12:59:22 +1100

    add some deps

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "https://github/rweir/gateau"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [cheshire "5.3.1"]
+                 [clj-logging-config "1.9.10"]
+                 [com.netflix.hystrix/hystrix-clj "1.3.13"]
+                 [http-kit "2.1.16"]])
